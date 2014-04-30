3c3b7773cd48e0591af8b7e547ef9ad1e77263b1
Tyler Ochiai <tyler.a.ochiai@gmail.com>
2014-04-30 14:31:49 -0700

    Prints timestamp with tweet

--- main.js
+++ main.js
@@ -6,9 +6,12 @@ $(document).ready(function(){
     var index = streams.home.length - 1;
      while(index >= 0 && index > streams.home.length - 10 ){
       var tweet = streams.home[index];
-      var $tweet = $('<div></div>');
-      $tweet.addClass("tweet");
-      $tweet.text('@' + tweet.user + ': ' + tweet.message + "\n");
+      var $tweet = $('<div class="tweet"></div>');
+      var $tweet_content = $('<div class="tweet-content"></div>');
+      var $tweet_timestamp = $('<div class="tweet-timestamp"></div>');
+      $tweet_content.text('@' + tweet.user + ': ' + tweet.message );
+      $tweet_timestamp.text(tweet.created_at);
+      $tweet.append($tweet_content, $tweet_timestamp);
       $tweet.appendTo($body);
       index -= 1;
     }
