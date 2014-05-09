df36896cea271f5b925aceb67faea621995047d0
Tyler Ochiai <tyler.a.ochiai@gmail.com>
2014-05-09 10:27:07 -0700

    Displays user on separate line from tweet content

--- main.js
+++ main.js
@@ -8,11 +8,13 @@ $(document).ready(function(){
      while(index >= 0 && index > streams.home.length - streamLength ){
       var tweet = streams.home[index];
       var $tweet = $('<div class="tweet"></div>');
+      var $tweet_author = $('<div class="tweet-author"></div>');
       var $tweet_content = $('<div class="tweet-content"></div>');
       var $tweet_timestamp = $('<div class="tweet-timestamp"></div>');
-      $tweet_content.text('@' + tweet.user + ': ' + tweet.message );
+      $tweet_author.text('@' + tweet.user)
+      $tweet_content.text(tweet.message );
       $tweet_timestamp.text(tweet.created_at);
-      $tweet.append($tweet_content, $tweet_timestamp);
+      $tweet.append($tweet_author, $tweet_content, $tweet_timestamp);
       $tweet.appendTo($body);
       index -= 1;
     }
