2fcbea87b69f3adb8536100d22c7970c08d7127b
Tyler Ochiai <tyler.a.ochiai@gmail.com>
2014-05-16 19:25:59 -0700

    Refactor tweet updates into function, update only tweet content rather than entire tweet element

--- main.js
+++ main.js
@@ -2,21 +2,29 @@ $(document).ready(function(){
   var $body = $('body');
   var $tweet_container = $('.tweet-container');
   var streamLength = 15;
-  setInterval(function() {
-    $(".tweet").remove();
+  var updateTweets = function() {
     var index = streams.home.length - 1;
+    var $currentTweet = $('.tweet').first();
      while(index >= 0 && index > streams.home.length - streamLength ){
       var tweet = streams.home[index];
-      var $tweet = $('<div class="tweet"></div>');
       var $tweet_author = $('<div class="tweet-author"></div>');
       var $tweet_content = $('<div class="tweet-content"></div>');
       var $tweet_timestamp = $('<div class="tweet-timestamp"></div>');
       $tweet_author.text('@' + tweet.user);
       $tweet_content.text(tweet.message );
       $tweet_timestamp.text(tweet.created_at);
-      $tweet.append($tweet_author, $tweet_content, $tweet_timestamp);
-      $tweet.appendTo($tweet_container);
+      $currentTweet.append($tweet_author, $tweet_content, $tweet_timestamp);
       index -= 1;
+      $currentTweet = $currentTweet.next();
     }
+  };
+  var initialLength = Math.min(streamLength, streams.home.length)
+  for (var i = 0; i < initialLength; i++) {
+    $tweet_container.append("<div class='tweet'></div>");
+  }
+  updateTweets();
+  setInterval(function() {
+    $(".tweet").children().remove();
+    updateTweets();
   }, 300);
 });
